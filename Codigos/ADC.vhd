af2163e85a73b79ca2177f1290ba483a9b1bbc09
Aldana Pocai <38792460+aldanapocai@users.noreply.github.com>
2022-08-25 16:42:56 -0300

    Se agregaron los comp voltimetro_dac, sigmadelta y se modificaron ADC y ffd_adc

--- Codigos/ADC.vhd
+++ Codigos/ADC.vhd
@@ -7,9 +7,9 @@ entity ADC is
     port(
         clk_i: in std_logic;
         rst_i: in std_logic;
-        ena_i: in std_logic;
+        ena_i: in std_logic;|
         D_i: in std_logic; --Voltaje positivo de entrada
-        Qn_o: out std_logic; --Voltaje negativo de salida
+        --Qn_o: out std_logic; --Voltaje negativo de salida ?
         Q_ADC: out std_logic; --Salida del modulo
     );
 end;
@@ -23,8 +23,9 @@ architecture ADC_arq of ADC is
                 rst_i     => rst_i,
                 ena_i     => ena_i,
                 D_i       => D_i,
-                Q_o       => Q_ADC,
-                Q_n       => Qn_o
+                --Q_n       => Qn_o, --?
+                Q_o       => Q_ADC
+
             );
 
 end;
--- Codigos/ffd_adc.vhd
+++ Codigos/ffd_adc.vhd
@@ -10,8 +10,9 @@ entity ffd_adc is
         rst_i: in std_logic;
         ena_i: in std_logic;
         D_i: in std_logic;
-        Q_o: out std_logic;
-        Q_n: out std_logic
+        --Q_n: out std_logic;
+        Q_o: out std_logic
+
     );
 end;
 
